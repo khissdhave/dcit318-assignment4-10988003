17954b4e9883b612673741075a122d807d9a4119
khissdhave <davidamediku28@gmail.com>
2024-07-08 13:47:36 +0000

    Commit for question 3 - Drawing Canvas

---
+++ Drawing_Canvas/Drawing_Canvas/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Drawing_Canvas
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.panelCanvas = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panelCanvas
+            // 
+            this.panelCanvas.BackColor = System.Drawing.Color.White;
+            this.panelCanvas.Location = new System.Drawing.Point(12, 52);
+            this.panelCanvas.Name = "panelCanvas";
+            this.panelCanvas.Size = new System.Drawing.Size(760, 397);
+            this.panelCanvas.TabIndex = 0;
+            this.panelCanvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelCanvas_MouseDown);
+            this.panelCanvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelCanvas_MouseMove);
+            this.panelCanvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panelCanvas_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Script", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label1.Location = new System.Drawing.Point(285, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 34);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Drawing Canvas";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panelCanvas);
+            this.Name = "Form1";
+            this.Text = "Drawing App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Panel panelCanvas;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Drawing_Canvas/Drawing_Canvas/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Drawing_Canvas
+{
+    public partial class Form1 : Form
+    {
+        private bool isDrawing;
+        private Point previousPoint;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panelCanvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDrawing = true;
+                previousPoint = e.Location;
+            }
+        }
+
+        private void panelCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                using (Graphics g = panelCanvas.CreateGraphics())
+                {
+                    g.DrawLine(Pens.Black, previousPoint, e.Location);
+                }
+                previousPoint = e.Location;
+            }
+        }
+
+        private void panelCanvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDrawing = false;
+            }
+        }
+        private void panelCanvas_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
